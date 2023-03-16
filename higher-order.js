685dec0f0f800a446ed5ffe0791fa8ffb11e0aae
bill <billmukenga01@gmail.com>
2023-03-17 00:01:27 +0300

    fix: changed arrow function

--- higher-order.js
+++ higher-order.js
@@ -19,9 +19,8 @@ const map = (array, actionOnArray) => {
     return newArray;
 } ;
 
-const multiplyBy2 = (input) =>{
-    return input*2;
-};
+const multiplyBy2 = (input) => input*2;
+
 
 
 map([1,2,3,4,5,6], multiplyBy2);
